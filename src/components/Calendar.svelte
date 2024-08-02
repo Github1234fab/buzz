5d715a5bc643c07482d8abf8ee10ab71f9694756
Github1234fab <fabien.marceau@gmail.com>
2024-08-02 23:30:31 +0200

    ok décalage date

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -1,108 +1,112 @@
 <script>
-  import { jsonDataByDate } from "../lib/store.js"; // Import the store
-  import { get } from 'svelte/store';
+        import { jsonDataByDate } from "../lib/store.js"; // Import the store
+        import { get } from "svelte/store";
 
-  let currentDate = new Date();
-  let selectedDate = new Date();
-  let today = new Date();
-  let days = [];
-  let eventsForSelectedDay = null;
+        let currentDate = new Date();
+        let selectedDate = new Date();
+        let today = new Date();
+        let days = [];
+        let eventsForSelectedDay = null;
 
-  function loadDays(date) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const firstDayOfMonth = new Date(year, month, 1);
-    const lastDayOfMonth = new Date(year, month + 1, 0);
-    const startDay = firstDayOfMonth.getDay() === 0 ? 6 : firstDayOfMonth.getDay() - 1;
-    const endDay = lastDayOfMonth.getDay() === 0 ? 6 : lastDayOfMonth.getDay() - 1;
+        function loadDays(date) {
+                const year = date.getFullYear();
+                const month = date.getMonth();
+                const firstDayOfMonth = new Date(year, month, 1);
+                const lastDayOfMonth = new Date(year, month + 1, 0);
+                const startDay = firstDayOfMonth.getDay() === 0 ? 6 : firstDayOfMonth.getDay() - 1;
+                const endDay = lastDayOfMonth.getDay() === 0 ? 6 : lastDayOfMonth.getDay() - 1;
 
-    days = [];
+                days = [];
 
-    for (let i = startDay; i > 0; i--) {
-      days.push(new Date(year, month, 1 - i));
-    }
+                for (let i = startDay; i > 0; i--) {
+                        days.push(new Date(year, month, 1 - i));
+                }
 
-    for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
-      days.push(new Date(year, month, i));
-    }
+                for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
+                        days.push(new Date(year, month, i));
+                }
 
-    for (let i = 1; i < 7 - endDay; i++) {
-      days.push(new Date(year, month + 1, i));
-    }
-  }
+                for (let i = 1; i < 7 - endDay; i++) {
+                        days.push(new Date(year, month + 1, i));
+                }
+        }
 
-  function handleDayClick(day) {
-    selectedDate = new Date(day); // Créer une nouvelle date à partir du jour cliqué
-    selectedDate.setHours(0, 0, 0, 0); // Assurez-vous que l'heure est réglée sur minuit pour éviter les problèmes de fuseau horaire
+        function handleDayClick(day) {
+                console.log("Clicked day (input):", day);
+                console.log("Type of clicked day:", typeof day);
+                selectedDate = new Date(day); // Créer une nouvelle date à partir du jour cliqué
+                console.log(selectedDate);
+                // selectedDate.setUTCHours(0, 0, 0, 0);
+                // console.log(selectedDate.setUTCHours(0, 0, 0, 0));
+                  selectedDate.setHours(selectedDate.getHours() - selectedDate.getTimezoneOffset() / 60);
+                const selectedDateISO = selectedDate.toISOString().slice(0, 10);
+                console.log(selectedDateISO);
+                eventsForSelectedDay = get(jsonDataByDate)[selectedDateISO] || [];
+                console.log("Date sélectionnée :", selectedDateISO);
+                console.log("Événements pour cette date :", eventsForSelectedDay);
+        }
 
-    const selectedDateISO = selectedDate.toISOString().slice(0, 10);
-    eventsForSelectedDay = get(jsonDataByDate)[selectedDateISO] || [];
-    console.log("Date sélectionnée :", selectedDateISO);
-    console.log("Événements pour cette date :", eventsForSelectedDay);
-  }
+        function goToNextMonth() {
+                currentDate.setMonth(currentDate.getMonth() + 1);
+                loadDays(currentDate);
+        }
 
-  function goToNextMonth() {
-    currentDate.setMonth(currentDate.getMonth() + 1);
-    loadDays(currentDate);
-  }
+        function goToPreviousMonth() {
+                currentDate.setMonth(currentDate.getMonth() - 1);
+                loadDays(currentDate);
+        }
 
-  function goToPreviousMonth() {
-    currentDate.setMonth(currentDate.getMonth() - 1);
-    loadDays(currentDate);
-  }
-
-  $: loadDays(currentDate);
+        $: loadDays(currentDate);
 </script>
 
 <div>
-  <button on:click={goToPreviousMonth}>Mois précédent</button>
-  <button on:click={goToNextMonth}>Mois suivant</button>
+        <button on:click={goToPreviousMonth}>Mois précédent</button>
+        <button on:click={goToNextMonth}>Mois suivant</button>
 </div>
 <p>Date actuelle formatée : {currentDate.toLocaleDateString("fr-FR", { month: "long", year: "numeric" })}</p>
 
 <div class="calendar">
-  {#each days as day}
-    <button
-      class="day
+        {#each days as day}
+                <button
+                        class="day
         {day.getMonth() !== currentDate.getMonth() ? 'other-month' : ''}
         {day.getTime() === selectedDate.getTime() ? 'selected' : ''}
         {day.toDateString() === today.toDateString() ? 'today' : ''}
         {jsonDataByDate && jsonDataByDate[day.toISOString().slice(0, 10)] ? 'has-data' : ''}
 
       "
-      on:click={() => handleDayClick(day)}
-    >
-      {day.getDate()}
-    </button>
-  {/each}
+                        on:click={() => handleDayClick(day)}
+                >
+                        {day.getDate()}
+                </button>
+        {/each}
 
-  {#if eventsForSelectedDay}
-    <div class="event-info">
-      <p>Il y a {eventsForSelectedDay.length} événement(s) prévu(s) pour cette date.</p>
-      {#each eventsForSelectedDay as event}
-        <p>{event.date}</p>
-        <p>{event.diffuseur}</p>
-            <p>{event.horaire}</p>
-                <p>{event.lieu}</p>
-                    <p>{event.tarif}</p>
-                        <p>{event.type}</p>
-      {/each}
-    </div>
-  {/if}
+        {#if eventsForSelectedDay}
+                <div class="event-info">
+                        <p>Il y a {eventsForSelectedDay.length} événement(s) prévu(s) pour cette date.</p>
+                        {#each eventsForSelectedDay as event}
+                                <p>{event.date}</p>
+                                <p>{event.diffuseur}</p>
+                                <p>{event.horaire}</p>
+                                <p>{event.lieu}</p>
+                                <p>{event.tarif}</p>
+                                <p>{event.type}</p>
+                        {/each}
+                </div>
+        {/if}
 </div>
 
 <style>
-  .day.has-data::after {
-    content: '*';
-    color: red;
-    margin-left: 5px;
-  }
-          .event-info {
-  /* Styles pour la zone d'information des événements */
-  border: 1px solid #ddd;
-  padding: 10px;
-  margin-top: 10px;
-}
+        .day.has-data::after {
+                content: "*";
+                color: red;
+                margin: 0 auto;
+        }
+        .event-info {
+                border: 1px solid #ddd;
+                padding: 10px;
+                margin-top: 10px;
+        }
         .calendar {
                 display: grid;
                 grid-template-columns: repeat(7, 1fr);
@@ -124,116 +128,3 @@
                 background-color: yellow;
         }
 </style>
-
-
-
-
-
-
-
-
-
-<!-- <script>
-  import { jsonDataByDate } from "../lib/store.js"; 
-  import { get } from 'svelte/store';
-
-  let currentDate = new Date();
-  let selectedDate = new Date();
-  let today = new Date();
-  let days = [];
-  let eventsForSelectedDay = null;
-
-  // Fonction pour s'assurer que les heures sont à zéro
-  function resetTime(date) {
-    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
-  }
-
-  function loadDays(date) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const firstDayOfMonth = new Date(year, month, 1);
-    const lastDayOfMonth = new Date(year, month + 1, 0);
-
-    const startDay = firstDayOfMonth.getDay() === 0 ? 6 : firstDayOfMonth.getDay() - 1;
-    const endDay = lastDayOfMonth.getDay() === 0 ? 6 : lastDayOfMonth.getDay() - 1;
-
-    days = [];
-
-    // Add days from previous month
-    for (let i = startDay; i > 0; i--) {
-      days.push(resetTime(new Date(year, month, 1 - i)));
-    }
-
-    // Add days of current month
-    for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
-      days.push(resetTime(new Date(year, month, i)));
-    }
-
-    // Add days from next month
-    for (let i = 1; i < 7 - endDay; i++) {
-      days.push(resetTime(new Date(year, month + 1, i)));
-    }
-  }
-
-  function handleDayClick(day) {
-    selectedDate = resetTime(day);
-
-    const selectedDateISO = selectedDate.toISOString().slice(0, 10);
-    eventsForSelectedDay = get(jsonDataByDate)[selectedDateISO] || [];
-    console.log("Date sélectionnée :", selectedDateISO);
-    console.log("Événements pour cette date :", eventsForSelectedDay);
-  }
-
-  function goToNextMonth() {
-    currentDate.setMonth(currentDate.getMonth() + 1);
-    loadDays(currentDate);
-  }
-
-  function goToPreviousMonth() {
-    currentDate.setMonth(currentDate.getMonth() - 1);
-    loadDays(currentDate);
-  }
-
-  $: loadDays(currentDate);
-</script>
-
-<div>
-  <button on:click={goToPreviousMonth}>Mois précédent</button>
-  <button on:click={goToNextMonth}>Mois suivant</button>
-</div>
-<p>Date actuelle formatée : {currentDate.toLocaleDateString("fr-FR", { month: "long", year: "numeric" })}</p>
-
-<div class="calendar">
-  {#each days as day}
-    <button
-      class="day
-        {day.getMonth() !== currentDate.getMonth() ? 'other-month' : ''}
-        {day.getTime() === selectedDate.getTime() ? 'selected' : ''}
-        {day.toDateString() === today.toDateString() ? 'today' : ''}
-        {get(jsonDataByDate)[day.toISOString().slice(0, 10)] ? 'has-data' : ''}
-      "
-      on:click={() => handleDayClick(day)}
-    >
-      {day.getDate()}
-    </button>
-  {/each}
-
-  {#if eventsForSelectedDay}
-    <div class="event-info">
-      <p>Il y a {eventsForSelectedDay.length} événement(s) prévu(s) pour cette date.</p>
-      {#each eventsForSelectedDay as event}
-        <p>{event.title}</p>
-        <p>{event.description}</p>
-      {/each}
-    </div>
-  {/if}
-</div>
-
-<style>
-  .day.has-data::after {
-    content: '*';
-    color: red;
-    margin-left: 5px;
-  }
-</style> -->
-
