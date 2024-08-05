900e573db4bd5a6cf43ec067ad1d9f4e2ba812fa
Github1234fab <fabien.marceau@gmail.com>
2024-08-05 18:34:21 +0200

    v9

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 
-// import { defineConfig } from "vite";
-// import { sveltekit } from "@sveltejs/kit/vite";
-
-
-// export default defineConfig({
-//         plugins: [sveltekit()],
-       
-// });
-
 import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { sveltekit } from "@sveltejs/kit/vite";
+
 
 export default defineConfig({
-        plugins: [svelte()],
-        build: {
-                rollupOptions: {
-                        external: ["assets/arrowUp.svg", "assets/arrowDown.svg"],
-                },
-        },
-        assetsInclude: ["**/*.svg"],
+        plugins: [sveltekit()],
+       
 });
+
+// import { defineConfig } from "vite";
+// import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// export default defineConfig({
+//         plugins: [svelte()],
+//         build: {
+//                 rollupOptions: {
+//                         external: ["assets/arrowUp.svg", "assets/arrowDown.svg"],
+//                 },
+//         },
+//         assetsInclude: ["**/*.svg"],
+// });
